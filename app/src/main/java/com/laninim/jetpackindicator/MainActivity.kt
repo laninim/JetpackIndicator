5209434d1277a320039e0bec807f2e8105bd0a0b
Mauro <Lanini>
2023-08-23 17:16:11 +0200

    Implements CircleIndicator

--- app/src/main/java/com/laninim/jetpackindicator/MainActivity.kt
+++ app/src/main/java/com/laninim/jetpackindicator/MainActivity.kt
@@ -3,18 +3,29 @@ package com.laninim.jetpackindicator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.laninim.jetpackindicator.ui.theme.JetpackIndicatorTheme
 import com.laninim.jetpack_indicator.ArcIndicator
+import com.laninim.jetpack_indicator.CircleIndicator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,26 +37,211 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
                 ) {
-                    Column(
-                        modifier = Modifier.padding(top = 64.dp),
+                    val pageSelected = remember {
+                        mutableStateOf(0)
+                    }
+
+                    Column (
                         horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        ArcIndicator(
-                            indicatorSize = 150.dp,
-                            progressValue = 23,
-                            maxProgressValue = 100,
-                            foregroundColor = Color.Green,
-                            glowLight = true,
-                            showPercentage = true,
-                            textStyle = TextStyle(
-                                fontSize = 34.sp,
-                                color = Color.White
-                            )
-                        )
+                    ){
+                        if(pageSelected.value == 0){
+                            arcIndicator()
+                            Row {
+                                Button(onClick = { pageSelected.value = 0 }) {
+                                    Text(text = "Arc Indicator")
+                                }
+
+                                Spacer(modifier = Modifier.width(10.dp))
+
+                                Button(onClick = { pageSelected.value = 1 }) {
+                                    Text(text = "Circle Indicator")
+                                }
+                            }
+                        }else{
+                            CircleIndicator()
+                            Row {
+                                Button(onClick = { pageSelected.value = 0 }) {
+                                    Text(text = "Arc Indicator")
+                                }
+
+                                Spacer(modifier = Modifier.width(10.dp))
+
+                                Button(onClick = { pageSelected.value = 1 }) {
+                                    Text(text = "Circle Indicator")
+                                }
+                            }
+                        }
+
                     }
                 }
             }
         }
+    }
+
+
+}
+
+@Composable
+fun arcIndicator() {
+    Column(
+        modifier = Modifier.padding(top = 64.dp, bottom = 64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ArcIndicator(
+            indicatorSize = 100.dp,
+            progressValue = 50,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            indicatorStrokeWidth = 30f,
+            textStyle = TextStyle(
+                color = Color.White
+            )
+
+        )
+
+        Box(modifier = Modifier, contentAlignment = Alignment.Center){
+            ArcIndicator(
+                indicatorSize = 150.dp,
+                progressValue = 23,
+                maxProgressValue = 100,
+                foregroundColor = Color.Green,
+                glowLight = true,
+                showPercentage = false,
+                indicatorStrokeWidth = 34f
+
+            )
+            Text("23 GB", style = TextStyle(
+                color = Color.White,
+                fontSize = 30.sp,
+            )
+            )
+        }
+
+        ArcIndicator(
+            indicatorSize = 100.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 20.sp,
+                color = Color.White
+            ),
+            indicatorStrokeWidth = 25f
+        )
+
+        ArcIndicator(
+            indicatorSize = 150.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 34.sp,
+                color = Color.Green,
+                fontFamily = FontFamily.Monospace
+            )
+        )
+
+        ArcIndicator(
+            indicatorSize = 150.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Red,
+            backgroundColor = Color.Yellow,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 34.sp,
+                color = Color.White
+            )
+        )
+    }
+}
+
+@Composable
+fun CircleIndicator() {
+    Column(
+        modifier = Modifier.padding(top = 64.dp, bottom = 64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircleIndicator(
+            indicatorSize = 100.dp,
+            progressValue = 50,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            indicatorStrokeWidth = 30f,
+            textStyle = TextStyle(
+                color = Color.White
+            )
+
+        )
+
+        Box(modifier = Modifier, contentAlignment = Alignment.Center){
+            CircleIndicator(
+                indicatorSize = 150.dp,
+                progressValue = 23,
+                maxProgressValue = 100,
+                foregroundColor = Color.Green,
+                glowLight = true,
+                showPercentage = false,
+                indicatorStrokeWidth = 34f
+
+            )
+            Text("23 GB", style = TextStyle(
+                color = Color.White,
+                fontSize = 30.sp,
+            )
+            )
+        }
+
+        CircleIndicator(
+            indicatorSize = 100.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 20.sp,
+                color = Color.White
+            ),
+            indicatorStrokeWidth = 25f
+        )
+
+        CircleIndicator(
+            indicatorSize = 150.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Green,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 34.sp,
+                color = Color.Green,
+                fontFamily = FontFamily.Monospace
+            )
+        )
+
+        CircleIndicator(
+            indicatorSize = 150.dp,
+            progressValue = 23,
+            maxProgressValue = 100,
+            foregroundColor = Color.Red,
+            backgroundColor = Color.Yellow,
+            glowLight = true,
+            showPercentage = true,
+            textStyle = TextStyle(
+                fontSize = 34.sp,
+                color = Color.White
+            )
+        )
     }
 }
 
---
+++ jetpack-indicator/src/main/java/com/laninim/jetpack_indicator/CircleIndicator.kt
@@ -0,0 +1,185 @@
+package com.laninim.jetpack_indicator
+
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.Dp
+import com.laninim.jetpack_indicator.exception.MissingTextStyle
+
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun CircleIndicator(
+    modifier : Modifier = Modifier,
+    indicatorSize : Dp,
+    backgroundColor : Color = Color.Gray,
+    foregroundColor : Color = Color.Red,
+    indicatorStrokeWidth: Float = 70f,
+    progressValue : Int,
+    maxProgressValue : Int,
+    glowLight : Boolean? = null,
+    showPercentage : Boolean,
+    textStyle: TextStyle? = null
+
+) {
+
+    val animatedIndicatorValue = remember { Animatable(initialValue = 0f) }
+
+    LaunchedEffect(key1 = progressValue){
+        animatedIndicatorValue.animateTo(progressValue.toFloat())
+    }
+
+    val percentage = (animatedIndicatorValue.value / maxProgressValue) * 100
+
+    val sweepAngle by animateFloatAsState(
+        targetValue = (3.6 * percentage).toFloat(),
+        animationSpec = tween(1000), label = "sweepangleanimation"
+    )
+
+    val textMeasurer = rememberTextMeasurer()
+
+
+    Canvas(modifier = modifier.size(indicatorSize)){
+        val componentSize = this.size / 1.25f
+        backgroundIndicator(
+            componentSize = componentSize,
+            indicatorStrokeWidth = indicatorStrokeWidth,
+            backgroundColor = backgroundColor
+        )
+
+        if(glowLight != null && glowLight == true){
+            drawBlur(
+                componentSize = componentSize,
+                indicatorStrokeWidth = indicatorStrokeWidth,
+                backgroundColor = foregroundColor,
+                sweepAngle = sweepAngle
+            )
+        }
+
+
+        foregroundIndicator(
+            componentSize = componentSize,
+            foregroundColor = foregroundColor,
+            sweepAngle = sweepAngle,
+            indicatorStrokeWidth = indicatorStrokeWidth
+        )
+
+        if(showPercentage){
+            //se la percentuale è attiva ma non è stato definito un textstyle.
+            if(textStyle == null){
+                throw MissingTextStyle("If u want show percentage.. need define a textstyle")
+            }
+            textPercentage(percentage.toInt(), textMeasurer, textStyle = textStyle
+            )
+        }
+
+    }
+}
+
+private fun DrawScope.backgroundIndicator(
+    componentSize : Size,
+    indicatorStrokeWidth : Float,
+    backgroundColor: Color,
+
+    ) {
+    drawArc(
+        color = backgroundColor,
+        useCenter = false,
+        startAngle = 90f,
+        sweepAngle = 360f,
+        size = componentSize,
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2 ,
+            y = (size.height - componentSize.height) / 2
+        ),
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        )
+    )
+}
+
+private fun DrawScope.drawBlur(
+    componentSize : Size,
+    indicatorStrokeWidth : Float,
+    backgroundColor: Color,
+    sweepAngle: Float
+
+) {
+    for(i in 0..200){
+        drawArc(
+            color = backgroundColor.copy(alpha =  i / 900f),
+            useCenter = false,
+            startAngle = 90f,
+            sweepAngle = sweepAngle,
+            size = componentSize,
+            topLeft = Offset(
+                x = (size.width - componentSize.width) / 2 ,
+                y = (size.height - componentSize.height) / 2
+            ),
+            style = Stroke(
+                width = indicatorStrokeWidth + ( 15 - i) * 20,
+                cap = StrokeCap.Round
+            )
+        )
+    }
+}
+
+private fun DrawScope.foregroundIndicator(
+    componentSize : Size,
+    sweepAngle : Float,
+    foregroundColor: Color,
+    indicatorStrokeWidth: Float
+){
+    drawArc(
+        color = foregroundColor,
+        useCenter = false,
+        startAngle = 90f,
+        sweepAngle = sweepAngle,
+        size = componentSize,
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2 ,
+            y = (size.height - componentSize.height) / 2
+        ),
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        )
+    )
+}
+
+@OptIn(ExperimentalTextApi::class)
+private fun DrawScope.textPercentage(
+    percentage : Int,
+    textMeasurer: TextMeasurer,
+    textStyle : TextStyle
+) {
+    val textLayoutResult =  textMeasurer.measure("$percentage%", style = textStyle)
+
+    drawText(
+        textLayoutResult = textLayoutResult,
+        topLeft = Offset(
+            x = (size.width - textLayoutResult.size.width) / 2f,
+            y = (size.height - textLayoutResult.size.height) / 2f
+        )
+    )
+}
